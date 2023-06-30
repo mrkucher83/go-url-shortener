12c31cc04604710c8f2fd43a589787bf9eaac9ac
alexanderkucher <mr.kucher83@mail.ru>
2023-06-30 12:37:01 +0400

    Implemented testing via github.com/stretchr/testify

--- README.md
+++ README.md
@@ -4,4 +4,6 @@
 ## libraries used in project:
 - go-redis: https://redis.io/docs/clients/go/
 - gin-gonic: https://gin-gonic.com/docs/
+- testify: https://github.com/stretchr/testify 
+(for testing redisServer you should have Redis installed on your computer and launched: $ redis-server)
 
--- store/store_service_test.go
+++ store/store_service_test.go
@@ -1,1 +1,25 @@
 package store
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+var testStoreService = &StorageService{}
+
+func init() {
+	testStoreService = InitializeStore()
+}
+
+func TestInsertionAndRetieval(t *testing.T) {
+	initialLink := "https://www.guru3d.com/news-story/spotted-ryzen-threadripper-pro-3995wx-processor-with-8-channel-ddr4,2.html"
+	shortUrl := "Jsz4k57oAX"
+
+	// Persist data mapping
+	SaveUrlMapping(shortUrl, initialLink)
+
+	// Retrieve initial URL
+	retrieveUrl := RetrieveInitialUrl(shortUrl)
+
+	assert.Equal(t, initialLink, retrieveUrl)
+}
